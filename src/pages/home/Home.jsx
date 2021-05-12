43cb164483c55702672588645d6f1c6ac058c0b6
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-05-12 10:35:25 +0200

    cretae basic Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
+import { fetchHighlights } from "../../redux";
+import { HighlightsList } from "../../containers";
+
 function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+  const disptach = useDispatch();
+  const highlights = useSelector((state) => state.highlights);
+
+  useEffect(() => {
+    disptach(fetchHighlights());
+  }, [disptach]);
+
+  const copyHighlights = highlights.highlights.slice(0, 2);
+
+  if (highlights.loading) return <h1>Loading</h1>;
+  if (highlights.error) return <h1>{highlights.error}</h1>;
+  if (highlights.highlights)
+    return (
+      <ul>
+        <HighlightsList highlights={copyHighlights} />;
+      </ul>
+    );
 }
 
 export default Home;
